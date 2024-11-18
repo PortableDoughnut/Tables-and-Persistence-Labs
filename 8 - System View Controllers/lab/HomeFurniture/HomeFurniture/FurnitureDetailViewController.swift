7f5fd395258376a9646176a5b924b0a4c768481d
PortableDoughnut <gwen.thelin@gmail.com>
2024-11-17 18:47:38 -0700

    Completed Home Furniture Sharing

--- 8 - System View Controllers/lab/HomeFurniture/HomeFurniture/FurnitureDetailViewController.swift
+++ 8 - System View Controllers/lab/HomeFurniture/HomeFurniture/FurnitureDetailViewController.swift
@@ -10,6 +10,8 @@ class FurnitureDetailViewController: UIViewController {
     @IBOutlet var furnitureTitleLabel: UILabel!
     @IBOutlet var furnitureDescriptionLabel: UILabel!
     
+	let imagePicker: UIImagePickerController = .init()
+	
     init?(coder: NSCoder, furniture: Furniture?) {
         self.furniture = furniture
         super.init(coder: coder)
@@ -22,6 +24,8 @@ class FurnitureDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+		imagePicker.delegate = self
+		
         updateView()
     }
     
@@ -38,12 +42,66 @@ class FurnitureDetailViewController: UIViewController {
         furnitureDescriptionLabel.text = furniture.description
     }
     
-    @IBAction func choosePhotoButtonTapped(_ sender: Any) {
-        
+    @IBAction func choosePhotoButtonTapped(_ sender: UIButton) {
+        getPhoto(sender)
     }
 
-    @IBAction func actionButtonTapped(_ sender: Any) {
-        
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        guard let image = photoImageView.image else {return}
+		
+		let activityController: UIActivityViewController = .init(activityItems: [image], applicationActivities: nil)
+		activityController.popoverPresentationController?.sourceView = sender
+		
+		present(activityController, animated: true, completion: nil)
     }
     
 }
+
+extension FurnitureDetailViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	func getPhoto(_ sender: UIButton) {
+		let alertController: UIAlertController = .init(title: "Chose image source",
+													   message: nil,
+													   preferredStyle: .actionSheet)
+		
+		let cancelAlertAction: UIAlertAction = .init(title: "Cancel", style: .cancel, handler: nil)
+		alertController.addAction(cancelAlertAction)
+		
+		if UIImagePickerController.isSourceTypeAvailable(.camera) {
+			let cameraAction: UIAlertAction = .init(
+				title: "Camera",
+				style: .default,
+				handler: { [self] action in
+					imagePicker.sourceType = .camera
+					self.present(imagePicker, animated: true, completion: nil)
+				}
+			)
+			alertController.addAction(cameraAction)
+		}
+			if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+				let photoLibraryAction: UIAlertAction = .init(
+					title: "Photo Library",
+					style: .default,
+					handler: { [self] action in
+						imagePicker.sourceType = .photoLibrary
+						self.present(imagePicker, animated: true, completion: nil)
+					})
+				alertController.addAction(photoLibraryAction)
+			}
+		
+		alertController.popoverPresentationController?.sourceView = sender
+		
+		present(alertController, animated: true, completion: nil)
+		}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		guard let furnitureImage = info[.originalImage] as? UIImage else { return }
+		
+		furniture?.imageData = furnitureImage.jpegData(compressionQuality: 0.9)
+		
+		photoImageView.image = furnitureImage
+	}
+	
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		dismiss(animated: true, completion: nil)
+	}
+	}
